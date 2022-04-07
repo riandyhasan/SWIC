c9713ac3b29d355603452607884f679dd27a74fd
riandyhasan <riandyhsn@gmail.com>
2022-04-07 14:33:17 +0700

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId:  process.env.FIREBASE_APP_ID,
-  measurementId:  process.env.FIREBASE_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyBXjg_oKfrxN0J_a8g0_Uik_anFq0OqJFY",
+  authDomain: "swic-dev-92afb.firebaseapp.com",
+  projectId: "swic-dev-92afb",
+  storageBucket: "swic-dev-92afb.appspot.com",
+  messagingSenderId: "183852797733",
+  appId: "1:183852797733:web:64ec8c6c22729548cb14cc",
+  measurementId: "G-W6G1LH9GHN"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
