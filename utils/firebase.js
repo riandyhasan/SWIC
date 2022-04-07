1e5be6b2b966ffd4815745fc4504ef40251b7d00
riandyhasan <riandyhsn@gmail.com>
2022-04-07 14:11:19 +0700

    adjust env

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfvFPL3oSeM-C-E1hhuwfWXAQpmL1kUow",
-  authDomain: "swic-2d4c2.firebaseapp.com",
-  projectId: "swic-2d4c2",
-  storageBucket: "swic-2d4c2.appspot.com",
-  messagingSenderId: "244675206905",
-  appId: "1:244675206905:web:7a7c4df43ba668d09acc28",
-  measurementId: "G-ERK02KL9YV",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId:  process.env.FIREBASE_APP_ID,
+  measurementId:  process.env.FIREBASE_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
